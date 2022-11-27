721a60669778276120551f356a740d1cd7947a6b
RjelAutentico <rodrigo.jara1901@alumnos.ubiobio.cl>
2022-11-26 23:18:06 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+//const mongoose = require('mongoose');
+//const cors = require('cors');
+const dotenv = require('dotenv');
+const { default: mongoose } = require('mongoose');
+
+const app = express();
+
+dotenv.config();
+
+const options = {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    autoIndex: true,
+    keepAlive: true,
+    poolSize: 10,
+    bufferMaxEntries: 0,
+    connectTimeoutMS: 10000,
+    socketTimeoutMS: 45000,
+    family: 4,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+}
+mongoose.connect(process.env.DB,options,(error)=>{
+    if(error){
+        console.log(error)
+    }else{
+        console.log("Connected yo database")
+    }
+})
+
+
+app.listen(process.env.PORT,() => {
+    console.log(`Server started on port ${process.env.PORT}`);
+})
+
